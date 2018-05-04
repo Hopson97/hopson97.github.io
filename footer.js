fa2c8e7df6aefccdc70404cde63698b6fd9f68fc
Hopson97 <mhopson@hotmail.co.uk>
2018-05-04 17:31:11 +0100

    load navbar and footer from .js file (DRY)

---
+++ footer.js
@@ -0,0 +1,13 @@
+document.getElementById("footer").insertAdjacentHTML('afterend',
+`
+    <footer>
+        <div class = "center">
+            <ul class = "menu-items">
+                <li><a href = "https://github.com/Hopson97"><img src = "img/github.jpg"  class = "round-img"alt = "GitHub" /></a></li>
+                <li><a href = "https://www.youtube.com/c/hopsonn"><img src = "img/youtube.jpg" alt= "YouTube" class = "round-img" /></a></li>
+                <li><a href = "https://www.reddit.com/user/hopson97"><img src = "img/reddit.jpg" alt = "Reddit" class = "round-img" /></a></li>
+            </ul>
+        </div>
+    </footer>
+`
+);
---
+++ navbar.js
@@ -0,0 +1,15 @@
+document.getElementById("navbar").insertAdjacentHTML('afterend',
+`
+    <header>
+        <nav class = "center">
+            <ul class = "menu-items">
+                <li class = "menu-button"><a href = "index.html">Homepage</a></li> &bullet;
+                <li class = "menu-button"><a href = "#">Projects</a></li> &bullet;
+                <li class = "menu-logo"><a href = "index.html"><img src = "img/logo.jpg" class = "round-img"/></a></li> &bullet;
+                <li class = "menu-button"><a href = "#">About</a></li> &bullet;
+                <li class = "menu-button"><a href = "#">Old Stuff</a></li>
+            </ul>
+        </nav>
+    </header>
+`
+);
