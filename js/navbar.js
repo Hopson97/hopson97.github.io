9d3e437aee1fa2d3bfe82e569badf5a4442a14a5
Hopson97 <mhopson@hotmail.co.uk>
2018-05-05 09:24:39 +0100

    begin work on projects page

--- js/navbar.js
+++ js/navbar.js
@@ -4,7 +4,7 @@ document.getElementById("navbar").insertAdjacentHTML('afterend',
         <nav class = "center">
             <ul class = "menu-items">
                 <li class = "menu-button"><a href = "index.html">Homepage</a></li> &bullet;
-                <li class = "menu-button"><a href = "#">Projects</a></li> &bullet;
+                <li class = "menu-button"><a href = "projects.html">Projects</a></li> &bullet;
                 <li class = "menu-logo"><a href = "index.html"><img src = "img/logo.jpg" class = "round-img"/></a></li> &bullet;
                 <li class = "menu-button"><a href = "#">About</a></li> &bullet;
                 <li class = "menu-button"><a href = "#">Old Stuff</a></li>
