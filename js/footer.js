f8b049b9a88344de8ff26d64093daf4f39c728db
Hopson97 <mhopson@hotmail.co.uk>
2018-05-05 08:08:49 +0100

    Move navbar CSS to main CSS file

--- js/footer.js
+++ js/footer.js
@@ -3,9 +3,14 @@ document.getElementById("footer").insertAdjacentHTML('afterend',
     <footer>
         <div class = "center">
             <ul class = "menu-items">
+            <!--
                 <li><a href = "https://github.com/Hopson97"><img src = "img/github.jpg"  class = "round-img"alt = "GitHub" /></a></li>
                 <li><a href = "https://www.youtube.com/c/hopsonn"><img src = "img/youtube.jpg" alt= "YouTube" class = "round-img" /></a></li>
                 <li><a href = "https://www.reddit.com/user/hopson97"><img src = "img/reddit.jpg" alt = "Reddit" class = "round-img" /></a></li>
+            -->
+                <li><a href = "https://github.com/Hopson97">GitHub</a></li> &bullet;
+                <li><a href = "https://www.youtube.com/c/hopsonn">YouTube</a></li> &bullet;
+                <li><a href = "https://www.reddit.com/user/hopson97">Reddit</a></li> 
             </ul>
         </div>
     </footer>
