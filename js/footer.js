e63d11c17ed9a765e1272c69cce30458b6d2e507
Hopson97 <mhopson@hotmail.co.uk>
2018-05-05 10:35:05 +0100

    prettier

--- js/footer.js
+++ js/footer.js
@@ -3,9 +3,9 @@ document.getElementById("footer").insertAdjacentHTML('afterend',
     <footer>
         <div class = "center">
             <ul class = "menu-items">
-                <li><a href = "https://github.com/Hopson97">GitHub</a></li> &bullet;
-                <li><a href = "https://www.youtube.com/c/hopsonn">YouTube</a></li> &bullet;
-                <li><a href = "https://www.reddit.com/user/hopson97">Reddit</a></li> 
+                <li><a href = "https://github.com/Hopson97" target="_blank">GitHub</a></li> &bullet;
+                <li><a href = "https://www.youtube.com/c/hopsonn" target="_blank">YouTube</a></li> &bullet;
+                <li><a href = "https://www.reddit.com/user/hopson97" target="_blank">Reddit</a></li> 
             </ul>
         </div>
     </footer>
---
+++ js/head.js
@@ -0,0 +1,9 @@
+document.getElementById("head").insertAdjacentHTML('afterend',
+`
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="img/favicon.ico" type="image/x-icon">
+`
+);
