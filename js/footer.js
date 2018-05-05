d19079b25fa84b879fc5fe29ef6835caf6a79698
Hopson97 <mhopson@hotmail.co.uk>
2018-05-05 11:15:09 +0100

    added more projects

--- js/footer.js
+++ js/footer.js
@@ -7,6 +7,7 @@ document.getElementById("footer").insertAdjacentHTML('afterend',
                 <li><a href = "https://www.youtube.com/c/hopsonn" target="_blank">YouTube</a></li> &bullet;
                 <li><a href = "https://www.reddit.com/user/hopson97" target="_blank">Reddit</a></li> 
             </ul>
+            <a href = "#top">Back to Top</a>
         </div>
     </footer>
 `
