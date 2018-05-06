07348891f2f832489c83fa8c63f28f91411f0719
Hopson97 <mhopson@hotmail.co.uk>
2018-05-06 14:48:41 +0100

    improve look of footer

--- js/footer.js
+++ js/footer.js
@@ -6,10 +6,14 @@ document.getElementById("footer").insertAdjacentHTML('afterend',
                 <li><a href = "https://github.com/Hopson97" target="_blank">GitHub</a></li> &bullet;
                 <li><a href = "https://www.youtube.com/c/hopsonn" target="_blank">YouTube</a></li> &bullet;
                 <li><a href = "https://www.reddit.com/user/hopson97" target="_blank">Reddit</a></li> &bullet;
-                <li><a href = "https://discord.gg/feKbBwS" target="_blank">Discord</a></li> 
-                </ul>
-            <a href = "#top">Back to Top</a>
+                <li><a href = "https://discord.gg/feKbBwS" target="_blank">Discord</a></li> &bullet;
+                <li><a href = "https://www.patreon.com/Hopson" target="_blank">Patreon</a></li>  &bullet;
+
+                <li><a href = "https://github.com/Hopson97/hopson97.github.io" target="_blank">Source</a></li>  &bullet;
+                <li><a href = "#">Back to Top</a></li> 
+            </ul>
         </div>
     </footer>
 `
 );
+
