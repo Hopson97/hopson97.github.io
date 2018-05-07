f5de1b84551bdadd2717623c1e99d0c8dae9dd8f
Hopson97 <mhopson@hotmail.co.uk>
2018-05-07 06:09:48 +0100

    improve the JS for generatting project DOM

--- js/project_dom_gen.js
+++ js/project_dom_gen.js
@@ -1,52 +1,3 @@
-//Represents a single projects on the Projects page
-class Project 
-{
-    constructor(title, id, date)
-    {
-        this.title = title;
-        this.id = id;
-        this.date = date;
-        this.links = null;
-        this.images     = new Array();
-        this.paragraphs = new Array();
-        this.lists      = new Array();
-
-        this.parentElement = document.createElement("div");
-
-    }
-
-    addParagraph(text) 
-    {
-        this.paragraphs.push(text);
-        return this;
-    }
-
-    addList(title, content)
-    {
-        this.lists.push({title: title, content: content});
-        return this;
-    }
-
-    addLinks(yt, github, download) 
-    {
-        this.links = {
-            yt: yt,
-            github: github,
-            download: download
-        }
-        return this;
-    }
-
-    addImage(link)
-    {
-        this.images.push({
-            alt: "Image of " + this.title,
-            link: link
-        });
-        return this;
-    }
-}
-
 //Finds a child element based on class name
 function findChild(element, childClassName) 
 {
@@ -85,6 +36,127 @@ function createElement(element, text)
     return e;
 }
 
+class ProjectExtLink
+{
+    constructor(title, location)
+    {
+        this.title = title;
+        this.location = location;
+    }
+}
+
+//Represents a single projects on the Projects page
+class Project 
+{
+    constructor(title, id, date)
+    {
+        this.title = title;
+        this.id = id;
+        this.date = date;
+
+
+        this.links = null;
+        this.images     = new Array();
+        this.paragraphs = new Array();
+        this.lists      = new Array();
+
+        this.parentElement = document.createElement("div");
+        this.parentElement.classList.add("project-container");
+        this.parentElement.id = id;
+
+        let titleElement = createElement("h2", title);
+        let dateElement  = createElement("p", date);
+        dateElement.classList.add("minor");
+
+        this.parentElement.appendChild(titleElement);
+        this.parentElement.appendChild(dateElement);
+    }
+
+    addParagraph(text) 
+    {
+        let paragraphElement = createElement("p", text);
+        this.parentElement.appendChild(paragraphElement);
+        //this.paragraphs.push(text);
+        return this;
+    }
+
+    addList(title, content)
+    {
+        let listTitleElement = createElement("h3", title);
+        let listElementParent = document.createElement("ul");
+        for (let item of content) {
+            let listItem = createElement("li", item);
+            listElementParent.appendChild(listItem);
+        }
+        this.parentElement.appendChild(listTitleElement);
+        this.parentElement.appendChild(listElementParent);
+
+        //this.lists.push({title: title, content: content});
+        return this;
+    }
+
+    /*
+        Takes an array ->
+        {
+            title: title of the link
+            location: location of href of the link
+        }
+    */
+    addLinks(links/*yt, github, download*/) 
+    {
+        let linksTitleElement = createElement("h3", "Links");
+        let listElementParent = document.createElement("ul");
+        listElementParent.classList.add("inline-list");
+        listElementParent.classList.add("project-links");
+
+        function addLink(link)
+        {
+            let listItem = document.createElement("li");
+            let anchorElement = createElement("a", link.title);
+            anchorElement.target = "_blank";
+            anchorElement.href = link.location;
+
+            listItem.appendChild(anchorElement);
+            listElementParent.appendChild(listItem);
+        }
+
+        for (let i = 0; i < links.length; i++) {
+            addLink(links[i]);
+            console.add
+        }
+        
+        this.parentElement.appendChild(linksTitleElement);
+        this.parentElement.appendChild(listElementParent);
+        /*
+        this.links = {
+            yt: yt,
+            github: github,
+            download: download
+        }*/
+        return this;
+    }
+
+    addImage(imageSource)
+    {
+        let imageParent = document.createElement("div");
+        imageParent.classList.add("project-image");
+
+        let imageElement = document.createElement("img");
+        imageElement.classList.add("contained-img");
+        imageElement.src = imageSource;
+        imageElement.alt = "Image of " + this.title;
+
+        imageParent.appendChild(imageElement);
+        this.parentElement.appendChild(imageParent);
+        /*
+        this.images.push({
+            alt: "Image of " + this.title,
+            link: link
+        });*/
+        return this;
+    }
+}
+
 function getProjectArray()
 {
     let projectList = new Array();
@@ -99,9 +171,9 @@ function getProjectArray()
                                 "Remove blocks",
                                 "Collision Detection",
                                 "Simple Inventory System"])
-        .addLinks("https://www.youtube.com/watch?v=Xq3isov6mZ8",
-                  "https://github.com/Hopson97/MineCraft-One-Week-Challenge",
-                  "https://drive.google.com/uc?authuser=0&id=1t7P-1dQc799ZmZXmUpejum2zk2Ue-scH&export=download")
+        .addLinks([ new ProjectExtLink("YouTube", "https://www.youtube.com/watch?v=Xq3isov6mZ8"),
+                    new ProjectExtLink("GitHub",   "https://github.com/Hopson97/MineCraft-One-Week-Challenge"),
+                    new ProjectExtLink("Download", "https://drive.google.com/uc?authuser=0&id=1t7P-1dQc799ZmZXmUpejum2zk2Ue-scH&export=download")])
         .addImage("img/mc_week1.jpg")
        // .addImage("img/mc_week2.jpg")
     );
@@ -122,109 +194,6 @@ function getProjectArray()
         .addParagraph("A bunch of arcade games created using C++/SFML.")
     );
     return projectList;
-}
-
-
-//Creates the HTML for all of the projects
-function createProjectsHTML(projectList)
-{
-    //Create the main project section
-    let projects = document.getElementById("projects");
-    for (proj of projectList) {
-        //Get a template
-        let obj = getTemplateClone();
-    
-        //Set title
-        let titleEle = findChild(obj.container, "project-title");
-        let title = document.createTextNode(proj.title);
-        titleEle.appendChild(title);
-        titleEle.id = proj.id;
-    
-        //Set date
-        let dateEle = findChild(obj.container, "minor");
-        let date = document.createTextNode(proj.date);
-        dateEle.appendChild(date);
-    
-        //Add paragraphs
-        let paraEleParent = findChild(obj.container, "project-paragraphs");
-        for (let para of proj.paragraphs) {
-            let paraEle = createElement("p", para);
-            paraEleParent.appendChild(paraEle);
-        }
-    
-        //Add lists 
-        let listParent = findChild(obj.container, "project-lists");
-        for (let list of proj.lists) {
-            let listTitle = createElement("h3", list.title);
-            let listEle = document.createElement("ul");
-            for (let item of list.content) {
-                let listItem = createElement("li", item);
-                listEle.appendChild(listItem);
-            }
-            listParent.appendChild(listTitle);
-            listParent.appendChild(listEle);
-        }
-    
-        //Add links
-        if (proj.links != null) {
-            let linksParent = findChild(obj.container, "project-links");
-            let linkTitle = createElement("h3", "Links");
-            let listParent = document.createElement("ul");
-            listParent.classList.add("inline-list");
-            listParent.classList.add("project-links");
-    
-            function createLink(name, location) {
-                let li = document.createElement("li");
-                let a  = createElement("a", name);
-    
-                a.target = "_blank";
-                a.href = location;
-                li.appendChild(a);
-               // addTextToElement(li, "");
-                listParent.appendChild(li);
-            }
-    
-            createLink("YouTube", proj.links.yt);
-            createLink("GitHub", proj.links.github);
-            createLink("Download", proj.links.download);
-            linksParent.appendChild(linkTitle);
-            linksParent.appendChild(listParent);
-        }
-    
-        //Add images
-        let imagesParent = findChild(obj.container, "project-images");
-        imagesParent.classList.add("center");
-        for (let img of proj.images) {
-            let imageContainer = document.createElement("div");
-            imageContainer.classList.add("project-image")
-
-            let imageElement = document.createElement("img");
-            imageElement.classList.add("contained-img");
-            imageElement.src = img.link;
-            imageElement.alt = img.alt;
-            imageContainer.appendChild(imageElement);
-            imagesParent.appendChild(imageContainer);
-        }/*
-        if (proj.images.length == 0) {
-            obj.container.removeChild(findChild(obj.container, "project-image"));
-        }
-        //Add image
-        if (proj.image != null) {
-            let image = document.createElement("img");
-            image.classList.add("contained-img");
-            imageParent.classList.add("center");
-            image.src = proj.image.link;
-            image.alt = proj.image.alt;
-            imageParent.appendChild(image);
-        }
-        else {
-            obj.container.removeChild(findChild(obj.container, "project-image"));
-        }*/
-    
-    
-        let clone = document.importNode(obj.temp.content, true);
-        projects.appendChild(clone);
-    }
 }
 
 //Creates the contents section of the Projects page
@@ -264,7 +233,14 @@ function generateProjectHTML()
 {
     let projectList = getProjectArray();
     createContentsHTML(projectList);
-    createProjectsHTML(projectList);
+
+    let projectParent = document.getElementById("projects");
+    for (let project of projectList) {
+        projectParent.appendChild(project.parentElement);
+    }
+
+
+    //createProjectsHTML(projectList);
 }
 
 generateProjectHTML() 
