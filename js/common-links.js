e75b5b2c008e63762201dab999ae4cce787e8c53
Hopson97 <mhopson@hotmail.co.uk>
2019-03-28 15:36:10 +0000

    add gmail logo

--- js/common-links.js
+++ js/common-links.js
@@ -17,3 +17,4 @@ addLink(firstRow, "https://www.youtube.com/", "yt");
 addLink(firstRow, "https://www.github.com/", "gh");
 addLink(firstRow, "https://www.netflix.com/browse/", "nf");
 addLink(firstRow, "https://www.smile.amazon.co.uk/", "am");
+addLink(firstRow, "https://mail.google.com/mail/u/3/#inbox", "gm");
