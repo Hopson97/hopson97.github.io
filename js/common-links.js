9e5c944eef9b3da30e7fc7f800b9e134c251dc5b
Hopson97 <mhopson@hotmail.co.uk>
2019-03-28 15:25:14 +0000

    links generated from js rather than direct html

---
+++ js/common-links.js
@@ -0,0 +1,19 @@
+"use strict"
+
+const firstRow = document.getElementById("link-row-1");
+
+function addLink(row, link, imageSource) {
+    const anchorElement = document.createElement("a");
+    const imageElement  = document.createElement("img");
+
+    anchorElement.href = link;
+    imageElement.src = `img/logo-${imageSource}.png`;
+
+    anchorElement.appendChild(imageElement);
+    row.appendChild(anchorElement);
+}
+
+addLink(firstRow, "https://www.youtube.com/", "yt");
+addLink(firstRow, "https://www.github.com/", "gh");
+addLink(firstRow, "https://www.netflix.com/browse/", "nf");
+addLink(firstRow, "https://www.smile.amazon.co.uk/", "am");
