c09bcb8467507f21fb2c8075dd64a00ac8be4d10
Hopson97 <mhopson@hotmail.co.uk>
2021-03-06 22:12:58 +0000

    Upgrade the website

--- assets/Projects.js
+++ assets/Projects.js
@@ -1,4 +1,24 @@
 
 window.addEventListener("load", _ => {
-    $("#")
+    const projectList = document.getElementById("project-list");
+    const projects = Array.from(document.getElementById("project-articles").childNodes);
+    projects.shift();
+    projects.pop();
+    console.log(projects);
+    for (const project of projects) {
+        if (project.nodeName !== "ARTICLE") {
+            continue;
+        }
+
+        const tagList = project.querySelector(".project-tags");
+        const tags = project.getAttribute("tags").split(" ");
+        for (const tag of tags) {
+            project.classList.add(tag)
+
+            const tagNode = document.createElement("div");
+            tagNode.textContent = tag;
+            tagNode.classList.add("project-tag");
+            tagList.appendChild(tagNode);
+        }
+    }
 });
