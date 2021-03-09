2e6ba47fa04c83ec3e01b87fa30a08f632f626fb
Hopson97 <mhopson@hotmail.co.uk>
2021-03-09 13:25:22 +0000

    Add wire tool strategy

---
+++ assets/js/Circuit/Tools.js
@@ -0,0 +1,47 @@
+"use strict"
+
+class Tool {
+    constructor(board) {
+        this.board = board;
+    }
+
+    onMouseMove(x, y) {}
+    onClick(x, y) {}
+
+    onDraw(ctx) {}
+
+    isValidSpot(x, y) {}
+}
+
+class WireTool extends Tool {
+    constructor(board) {
+        super(board);
+
+        this.activeWire = null;
+    }
+
+    onMouseMove(x, y) {
+        if (this.activeWire) {
+            this.activeWire.endX = x;
+            this.activeWire.endY = y;
+        }
+    }
+
+    onClick(x, y) {
+        if (this.activeWire) {
+            this.board.addWire(this.activeWire);
+            this.activeWire = null;
+        } else {
+            this.activeWire = new Wire(x, y);
+            this.board.getHole(x - 2, y - 8).active = true;
+        }
+    }
+
+    onDraw(ctx) {
+        if (this.activeWire) {
+            this.activeWire.draw(ctx, this.x, this.y);
+        }
+    }
+
+    isValidSpot(x, y) {}
+}
