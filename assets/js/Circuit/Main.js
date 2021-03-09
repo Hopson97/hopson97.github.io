a4afcd29f8a301c3bc94f9fa5de7ff0534ee4611
Hopson97 <mhopson@hotmail.co.uk>
2021-03-09 12:27:57 +0000

    Created board class

--- assets/js/Circuit/Main.js
+++ assets/js/Circuit/Main.js
@@ -31,45 +31,6 @@ function idx(x, y) {
     return y * BOARD_WIDTH + x;
 }
 
-class Board {
-    constructor() {
-        this.map = [];
-        for (let y = 0; y < BOARD_HEIGHT; y++) {
-            for (let x = 0; x < BOARD_WIDTH; x++) {
-                const hole = {
-                    active: false
-                };
-                this.map.push(hole);
-                if (layout[idx(x, y)] == 1) {
-                    hole.active = true;
-                }
-            }
-        }
-    }
-
-    render(ctx) {
-        for (let y = 0; y < BOARD_HEIGHT; y++) {
-            for (let x = 0; x < BOARD_WIDTH; x++) {
-                if (layout[idx(x, y)] == 1) {
-                    ctx.drawRect(
-                        x * HOLE_SIZE + HOLE_SIZE * x,
-                        y * HOLE_SIZE + HOLE_SIZE * y,
-                        HOLE_SIZE, HOLE_SIZE,
-                        "#DDD", "#FFF");
-                }
-            }
-        }
-    }
-}
-
-const fpsInterval = 1000 / 30;
-let then = Date.now();
-let start = then;
-let now = 0;
-let elapsed = 0;
-
-let drawwire = false;
-
 class Wire {
     constructor(mx, my) {
         this.startX = mx;
@@ -88,7 +49,7 @@ class Wire {
         const endY = this.endY * HOLE_SIZE * 2 + HOLE_SIZE / 2;
 
         const controlX = (endX + startX) / 2;
-        const controlY = endY - 50;
+        const controlY = (startY + startY) / 2;
 
         ctx.drawCurve(
             startX, startY,
@@ -98,16 +59,83 @@ class Wire {
     }
 }
 
-let currentWire = null;
+class Board {
+    constructor() {
+        this.wires = [];
+        this.currentWire = null;
+
+        this.x = HOLE_SIZE * 4;
+        this.y = HOLE_SIZE * 16;
+        this.map = [];
+        this.nodeMCUImage = new Image();
+        this.button = new Image();
+        this.nodeMCUImage.src = "assets/img/circuit/esp.png";
+        this.button.src = "assets/img/circuit/button.png";
+
+        for (let y = 0; y < BOARD_HEIGHT; y++) {
+            for (let x = 0; x < BOARD_WIDTH; x++) {
+                const hole = {
+                    active: false
+                };
+                this.map.push(hole);
+                if (layout[idx(x, y)] == 1) {
+                    hole.active = true;
+                }
+            }
+        }
+    }
+
+    render(ctx) {
+        ctx.drawRect(this.x - 10, this.y - 10, 1520, 420, "white");
+        ctx.drawRect(this.x, this.y + 400 / 2 - HOLE_SIZE / 2, 1500, HOLE_SIZE, "#DDD","#DDD");
+        ctx.drawRect(this.x, this.y + HOLE_SIZE * 4, 1500, HOLE_SIZE / 2, "#DDD","#DDD");
+        ctx.drawRect(this.x, this.y + HOLE_SIZE * 28, 1500, HOLE_SIZE / 2, "#DDD","#DDD");
+
+
+
+
+        for (let y = 0; y < BOARD_HEIGHT; y++) {
+            for (let x = 0; x < BOARD_WIDTH; x++) {
+                if (layout[idx(x, y)] == 1) {
+                    ctx.drawRect(
+                        x * HOLE_SIZE + HOLE_SIZE * x + this.x,
+                        y * HOLE_SIZE + HOLE_SIZE * y + this.y,
+                        HOLE_SIZE, HOLE_SIZE,
+                        "#222", "#333");
+                }
+            }
+        }
+        ctx.drawImage(this.nodeMCUImage,
+            HOLE_SIZE * 94 + this.x,
+            HOLE_SIZE * 8 - 1 + this.y,
+            414, 205
+        );
+        ctx.drawImage(this.button,
+            HOLE_SIZE + this.x, HOLE_SIZE * 13.5 + this.y,
+            36 * 2, 36 * 2
+        );
+
+
+        if (this.currentWire) {
+            this.currentWire.draw(ctx, this.x, this.y);
+            console.log(this.currentWire);
+        }
+        for (const wire of this.wires) {
+            wire.draw(ctx, this.x, this.y);
+        }
+    }
+}
+
+
+
 
 
 window.addEventListener("load", () => {
     const canvas = document.getElementById("circuitcanvas");
     const ctx = new Context(canvas);
-    const nodeMCUImage = new Image();
-    nodeMCUImage.src = "assets/img/circuit/esp.png";
 
-    const wires = [];
+
+    this.board = new Board();
 
     let mx = 0;
     let my = 0
@@ -117,58 +145,44 @@ window.addEventListener("load", () => {
         const y = e.offsetY;
         mx = Math.floor(x / HOLE_SIZE / 2);
         my = Math.floor(y / HOLE_SIZE / 2);
-        if (currentWire) {
-            currentWire.endX = mx;
-            currentWire.endY = my;
+        if (board.currentWire) {
+            board.currentWire.endX = mx;
+            board.currentWire.endY = my;
         }
     });
 
     canvas.addEventListener("mouseup", (e) => {
-        if (currentWire) {
-            wires.push(currentWire);
-            currentWire = null;
-        }
-        else {
+        if (board.currentWire) {
+            board.wires.push(board.currentWire);
+            board.currentWire = null;
+        } else {
             const x = e.offsetX;
             const y = e.offsetY;
             mx = Math.floor(x / HOLE_SIZE / 2);
             my = Math.floor(y / HOLE_SIZE / 2);
-            currentWire = new Wire(mx, my);
+            board.currentWire = new Wire(mx, my);
         }
     });
     // D0  D1  D2  D3  D4  3V3 GND D5  D6  D7  D8  RX  TX  GND 3V3
 
     // A0 RSV RSV SD3 SD2 SD1 CMD SD0 CLK GND 3V3  EN RST GND Vin 
 
-    this.board = new Board();
-    ctx.font = "30px Verdana";
 
     function loop() {
-        ctx.drawRect(
-            0, 0,
-            WIDTH, HEIGHT,
-            "#000", "#000");
+        // clear
+        ctx.clear();
+
         board.render(ctx);
 
-        ctx.drawImage(nodeMCUImage,
-            HOLE_SIZE * 94,
-            HOLE_SIZE * 8 - 1,
-            414, 205);
+
 
         ctx.drawRect(
             mx * HOLE_SIZE + HOLE_SIZE * mx,
             my * HOLE_SIZE + HOLE_SIZE * my,
             HOLE_SIZE, HOLE_SIZE,
             "#F00", "#FFF");
-        ctx.render();
 
 
-        if (currentWire) {
-            currentWire.draw(ctx);
-        }
-        for (const wire of wires) {
-            wire.draw(ctx);
-        }
 
 
         requestAnimationFrame(loop);
